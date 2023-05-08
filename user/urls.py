355ade4bb79055e554453aadd2c18df89712b78e
Artem Kazakov <artemkazakov947@gmail.com>
2023-05-08 11:39:18 +0200

    Added endpoint to login

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from user.views import CreateUserView, UpdateUserView
+from user.views import CreateUserView, UpdateUserView, CreateTokenView
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create-user"),
     path("me/", UpdateUserView.as_view(), name="update-user"),
+    path("login/", CreateTokenView.as_view())
 ]
 
 app_name = "user"
