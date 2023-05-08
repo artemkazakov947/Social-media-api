7acb1e4bd263ae8c79ea20aef96580e95f44c01e
Artem Kazakov <artemkazakov947@gmail.com>
2023-05-08 11:38:13 +0200

    Added authentication_class attribute to UpdateUserView

--- user/views.py
+++ user/views.py
@@ -1,4 +1,7 @@
 from rest_framework import generics
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from user.serializers import UserSerializer
 
@@ -9,6 +12,11 @@ class CreateUserView(generics.CreateAPIView):
 
 class UpdateUserView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
+    authentication_classes = (TokenAuthentication,)
 
     def get_object(self):
         return self.request.user
+
+
+class CreateTokenView(ObtainAuthToken):
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
