67be19c33b90919dd8d9b1e0a7a0c34e101be5ce
Artem Kazakov <artemkazakov947@gmail.com>
2023-05-10 10:15:11 +0200

    Profile model was created

--- social_media/models.py
+++ social_media/models.py
@@ -1,3 +1,35 @@
+import os.path
+import uuid
+
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+def profile_image_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+
+    file = f"{instance.user.email}-{uuid.uuid4()}.{extension}"
+
+    return os.path.join("uploads/profiles/", file)
+
+
+class Profile(models.Model):
+    class SexChoices(models.TextChoices):
+        MAN = "Man"
+        WOMAN = "Woman"
+        ANOTHER = "Another"
+
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="user")
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    nick_name = models.CharField(max_length=255)
+    sex = models.CharField(max_length=7, choices=SexChoices.choices, default=SexChoices.ANOTHER)
+    registered = models.DateField(auto_now_add=True)
+    bio = models.TextField()
+    image = models.ImageField(null=True, upload_to=profile_image_file_path)
+
+    class Meta:
+        unique_together = ("last_name", "nick_name",)
+
+    def __str__(self):
+        return f"{self.first_name} {self.nick_name} {self.last_name}"
--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework.authtoken",
     "user",
+    "social_media",
 ]
 
 MIDDLEWARE = [
@@ -124,6 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media/"
 
 AUTH_USER_MODEL = "user.User"
 LOGIN_REDIRECT_URL = "/"
