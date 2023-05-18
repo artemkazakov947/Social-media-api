b50425976fa25a5906d29f8e5aed9da0fa1fbe85
Artem Kazakov <artemkazakov947@gmail.com>
2023-05-18 10:22:39 +0200

    Added model Post

--- social_media/models.py
+++ social_media/models.py
@@ -3,14 +3,23 @@ import uuid
 
 from django.conf import settings
 from django.db import models
+from user.models import User
 
 
-def profile_image_file_path(instance, filename):
+def profile_image_file_path(instance, filename: str):
     _, extension = os.path.splitext(filename)
 
     file = f"{instance.user.email}-{uuid.uuid4()}.{extension}"
 
     return os.path.join("uploads/profiles/", file)
+
+
+def post_image_file_path(instance, filename: str):
+    _, extension = os.path.splitext(filename)
+
+    file = f"{instance.user.email}-{uuid.uuid4()}.{extension}"
+
+    return os.path.join("uploads/posts/", file)
 
 
 class Profile(models.Model):
@@ -48,3 +57,17 @@ class Follow(models.Model):
 
     class Meta:
         unique_together = ("follower", "following")
+
+
+class Post(models.Model):
+    topic = models.CharField(max_length=255)
+    text = models.TextField()
+    image = models.ImageField(null=True, upload_to=post_image_file_path)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="posts")
+    pub_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-pub_date"]
+
+    def __str__(self):
+        return f"Author: {self.user}, date: {self.pub_date}"
