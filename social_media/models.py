827d09507e6eb1fbb0aa623a48c77071c82f37d9
Artem Kazakov <artemkazakov947@gmail.com>
2023-06-12 18:46:37 +0200

    Added Comment model

--- social_media/models.py
+++ social_media/models.py
@@ -78,3 +78,17 @@ class Post(models.Model):
 
     def __str__(self):
         return f"Author: {self.user}, date: {self.created}"
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="comments")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created"]
+
+    def __str__(self):
+        return f"Comment by {self.author} to post id: {self.post.id}"
