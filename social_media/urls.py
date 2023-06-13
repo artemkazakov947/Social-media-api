6b872a9894a48d42e2c7e9055b324f547c1acbfe
Artem Kazakov <artemkazakov947@gmail.com>
2023-06-13 12:29:17 +0200

    Added nested routing for comments

--- social_media/urls.py
+++ social_media/urls.py
@@ -1,11 +1,29 @@
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from social_media.views import ProfileViewSet, PostViewSet
+from rest_framework_nested import routers
+
+from social_media.views import ProfileViewSet, PostViewSet, CommentViewSet
 
 router = DefaultRouter()
 
 router.register("profiles", ProfileViewSet)
 router.register("posts", PostViewSet)
 
-urlpatterns = router.urls
+comment_router = routers.NestedSimpleRouter(
+    router,
+    r"posts",
+    lookup="post"
+)
+
+comment_router.register(
+    r"comments",
+    CommentViewSet,
+    basename="post-comment"
+)
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path("", include(comment_router.urls))
+]
 
 app_name = "social_media"
