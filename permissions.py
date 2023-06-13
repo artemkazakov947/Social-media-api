fd9efccba1c3984cdaeebaa4aba6781529afa8af
Artem Kazakov <artemkazakov947@gmail.com>
2023-06-13 12:18:17 +0200

    Added to the permissions.IsOwnerOrReadOnly case for comments

--- permissions.py
+++ permissions.py
@@ -1,9 +1,14 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
+from social_media.models import Profile, Comment
+
 
 class IsOwnerOrReadOnly(BasePermission):
-    def has_object_permission(self, request, view, profile):
+    def has_object_permission(self, request, view, obj):
         if request.method in SAFE_METHODS:
             return True
+        if isinstance(obj, Profile):
+            return obj.user == request.user
 
-        return profile.user == request.user
+        elif isinstance(obj, Comment):
+            return obj.author == request.user
