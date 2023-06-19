80b7e6b32c43cf276c29e35d6c7add294ed8f473
Artem Kazakov <artemkazakov947@gmail.com>
2023-06-19 10:31:24 +0200

    added schedule task

---
+++ social_media/tasks.py
@@ -0,0 +1,15 @@
+from celery import shared_task
+from django.contrib.auth import get_user_model
+
+from social_media.models import Post
+
+
+@shared_task
+def schedule_post():
+    admin = get_user_model().objects.get(email="admin@admin.com")
+    text = "Time to donate to ZSU"
+    Post.objects.create(
+        topic="daily task",
+        text=text,
+        user=admin
+    )
